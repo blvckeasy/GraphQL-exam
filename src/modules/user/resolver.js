bef0abcce419af4d2b8a7e70fb64f790e780a494
blvckeasy <abdurakhmonovislom9@gmail.com>
2022-02-10 11:12:08 +0500

    User module added scalar types Email, Contact and one type enum Role

--- src/modules/user/resolver.js
+++ src/modules/user/resolver.js
@@ -1,6 +1,57 @@
+const { GraphQLScalarType, Kind } = require('graphql')
 const model = require('./model.js')
 
+const contactScalar = new GraphQLScalarType({
+  name: 'Contact',
+  description: 'This is a string for representing contacts',
+  serialize: checkContact,
+  parseValue: checkContact,
+  parseLiteral: function (AST) {
+    if (AST.kind == Kind.STRING) {
+      return checkContact(AST.value)
+    } else throw new Error('Contact value must String!')
+  },
+})
+
+function checkContact(value) {
+  if (!(typeof value == 'string'))
+    throw new Error('Contact value must be String!')
+  if (!/^[+]998[389][012345789][0-9]{7}$/.test(value))
+    throw new Error('Contact value must be valid contact!')
+
+  return value
+}
+
+const emailScalar = new GraphQLScalarType({
+  name: 'Email',
+  description: 'This is a string for representing Email',
+  serialize: checkEmail,
+  parseValue: checkEmail,
+  parseLiteral: function (AST) {
+    if (AST.kind == Kind.STRING) {
+      return checkEmail(AST.value)
+    } else throw new Error('Email value must String!')
+  },
+})
+
+function checkEmail(value) {
+  if (!(typeof value == 'string'))
+    throw new Error('Email value must be String!')
+  if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(value))
+    throw new Error('Email value must be valid email!')
+
+  return value
+}
+
 module.exports = {
+  Contact: contactScalar,
+  Email: emailScalar,
+
+  Role: {
+    admin: true,
+    user: false
+  },
+
   Query: {
     users: (_, { id, username, contact, email, role }) =>
       model.users({ id, username, contact, email, role }),
