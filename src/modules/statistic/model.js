f1ae261a7917e174bed49fb86b3abbf756a87e3a
blvckeasy <abdurakhmonovislom9@gmail.com>
2022-02-11 15:48:28 +0500

    statistic module version-2

--- src/modules/statistic/model.js
+++ src/modules/statistic/model.js
@@ -6,10 +6,18 @@ const STATISTIC = `
 	FROM orders o
   LEFT JOIN products p ON o.product_id = p.id
 	WHERE	
-    o.is_paid = $1
+    o.is_paid = $1 and
+    CASE 
+      WHEN length($2) > 0 THEN $2::timestamp <= o.order_created_at
+      ELSE TRUE
+    END AND
+    CASE 
+      WHEN length($3) > 0 THEN $3::timestamp >= o.order_created_at
+      ELSE TRUE
+    END
 `
+const statistic = ({ paid, toDate, fromDate }) => fetchAll(STATISTIC, paid, toDate, fromDate)
 
-const statistic = ({ paid }) => fetchAll(STATISTIC, paid)
 
 module.exports = {
   statistic,
--- src/modules/statistic/resolver.js
+++ src/modules/statistic/resolver.js
@@ -5,9 +5,10 @@ module.exports = {
   Query: {
     statistic: async (_, { paid, toDate, fromDate }) => {
       const price = await model.statistic({ paid, toDate, fromDate })
+      if (!price.length) return { price: 0 }
       return { price: price.reduce( (a, b) => a.price + b.price )}
     },
-  },
+},
 
   Price: {
     price: parent => parent.price
